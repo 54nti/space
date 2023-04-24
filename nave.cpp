ee4d141fefd3c028dcb565e694ac5806c2228d52
santi <santi.ledes.99@gmail.com>
2023-04-24 08:26:35 -0300

    implementing

--- nave.cpp
+++ nave.cpp
@@ -7,5 +7,20 @@ void Nave::setup() {
 
 void Nave::draw() {
     mvaddch(m_y, m_x + 2, '^');
-    mvaddch(m_y + 1, m_x + 1, '^');
+    mvaddch(m_y + 1, m_x + 1, '(');
+    mvaddch(m_y + 1, m_x + 2, ACS_PLMINUS);
+    mvaddch(m_y + 1, m_x + 3, ')');
+    mvaddch(m_y + 2, m_x, ACS_ULCORNER);
+    mvaddch(m_y + 2, m_x + 1, '#');
+    mvaddch(m_y + 2, m_x + 2, ' ');
+    mvaddch(m_y + 2, m_x + 3, '#');
+    mvaddch(m_y + 2, m_x + 4, ACS_URCORNER);
 }
+
+void Nave::setX(int x) { m_x = x; }
+
+void Nave::setX(int y) { m_y = y; }
+
+int Nave::getX() { return m_x; }
+
+int Nave::getX() { return m_x; }
