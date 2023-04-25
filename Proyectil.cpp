9524b2cd819a3f17abf060abb1d09498ded4b5fc
santi <santi.ledes.99@gmail.com>
2023-04-25 10:20:30 -0300

    add Proyectil

---
+++ Proyectil.cpp
@@ -0,0 +1,5 @@
+//
+// Created by usuario on 25/04/23.
+//
+
+#include "Proyectil.h"
---
+++ Proyectil.h
@@ -0,0 +1,14 @@
+//
+// Created by usuario on 25/04/23.
+//
+
+#ifndef SPACE_PROYECTIL_H
+#define SPACE_PROYECTIL_H
+
+
+class Proyectil {
+
+};
+
+
+#endif //SPACE_PROYECTIL_H
